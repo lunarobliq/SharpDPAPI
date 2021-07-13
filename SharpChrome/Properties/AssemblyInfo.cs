804abda74983e4929d2c21b456e7964897206aae
ceramicskate0 <64501203+ceramic-skate0@users.noreply.github.com>
2021-07-13 12:55:21 -0400

    Update AssemblyInfo.cs

--- SharpChrome/Properties/AssemblyInfo.cs
+++ SharpChrome/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SharpChrome")]
+[assembly: AssemblyTitle("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SharpChrome")]
+[assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
